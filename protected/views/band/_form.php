577cabac6f02e2f649fbc77ab7f860146e662e11
Adam Carter <adamcarter@jakatasalon.co.uk>
2013-09-09 09:01:36 +0100

    added band form

--- protected/views/band/_form.php
+++ protected/views/band/_form.php
@@ -4,11 +4,15 @@
 /* @var $form CActiveForm */
 ?>
 
+<?php Yii::app()->clientScript->registerCoreScript('jquery'); ?>
+
 <div class="form">
 
 <?php $form=$this->beginWidget('CActiveForm', array(
 	'id'=>'band-form',
 	'enableAjaxValidation'=>false,
+	'htmlOptions'=>array(
+		'onsubmit'=>"return false;")/* Disable normal form submit */
 )); ?>
 
 	<p class="note">Fields with <span class="required">*</span> are required.</p>
@@ -28,9 +32,38 @@
 	</div>
 
 	<div class="row buttons">
-		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+		 <?php echo CHtml::Button('SUBMIT',array('onclick'=>'send();')); ?> 
 	</div>
 
 <?php $this->endWidget(); ?>
 
 </div><!-- form -->
+
+<script type="text/javascript">
+ 
+function send()
+ {
+ 
+   var data=$("#band-form").serialize();
+ 
+ 
+  $.ajax({
+   type: 'POST',
+    url: '<?php echo Yii::app()->createAbsoluteUrl("band/create"); ?>',
+   data:data,
+success:function(data){
+                $("#result").html(data); 
+              },
+   error: function(data) { // if error occured
+         alert("Error occured.please try again");
+         alert(data);
+    },
+  dataType:'html'
+  });
+ 
+}
+ 
+</script>
+
+
+<div id="result"></div>
