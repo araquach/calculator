d8dfd8c58cc4d8a2fd019524f4b572caab05a357
Araquach <adam@contra-design.com>
2013-09-10 16:43:47 +0100

    updated the js

--- scripts/button.js
+++ scripts/button.js
@@ -4,26 +4,24 @@ $(document).ready(function() {
  // see if anything is previously checked and reflect that in the view
 $(".checklist input:checked").parent().addClass("selected");
 // hide the radio
-$(".checklist input").css('display','none');
+$(".checklist input").css('display','none')
 // handle the user selections
-var summary = $("#summary");
 $(".checklist .checkbox-select").click(
 function(event) {
-$this = $(this);
 event.preventDefault();
-$this.parent().addClass("selected");
-$this.parent().find(":radio").attr('checked',true);
-var clone = $this.clone();
-summary.append(clone);
+$(this).parent().addClass("selected");
+$(this).parent().find(":radio").attr('checked',true);
+$(this).parent().siblings().removeClass("selected");
+ 
 }
 );
  
 $(".checklist .checkbox-deselect").click(
 function(event) {
-$this = $(this);
 event.preventDefault();
-$this.parent().removeClass("selected");
-$this.parent().find(":radio").attr("checked",false);
+$(this).parent().removeClass("selected");
+$(this).parent().find(":radio").attr("checked",false);
+ 
 }
 );
  
